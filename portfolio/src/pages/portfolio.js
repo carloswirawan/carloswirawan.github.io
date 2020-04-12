1184a1dfaae3ca2447b35a8ddcef9b0c9ae5b07a
cawirawa <57200820+cawirawa@users.noreply.github.com>
2020-04-12 17:19:23 +0700

    modified

---
+++ portfolio/src/pages/portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../css/portfolio.css";
+class Portfolio extends Component{
+    render(){
+
+        return (
+            <div className="Portfolio">
+                <h1 id="Header">Carlos Wirawan's Portfolio</h1>
+            </div>
+        );
+    }
+}
+
+export default Portfolio;
