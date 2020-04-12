b57f8726ec35c4429a0df29e8fb6805246ff7caa
cawirawa <57200820+cawirawa@users.noreply.github.com>
2020-04-12 15:46:12 +0700

    React

---
+++ portfolio/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Redirect } from 'react-router-dom'
+
+class App extends Component {
+  state = {
+    redirect: false,
+  }
+
+  setRedirect = () => {
+    this.setState({redirect: true});
+  }
+
+  render() {
+    if (this.state.redirect)
+      return <Redirect to='/portfolio' />
+      
+    return (
+      <div className="App">
+        <title>Welcome!</title>}
+        <h1 id="Welcome">Welcome</h1>
+        <button title="See Carlos' portfolio" onClick={this.setRedirect}>Portfolio</button>
+      </div>
+    )
+  };
+}
+
+export default App;
---
+++ portfolio/src/pages/portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../css/portfolio.css";
+class Portfolio extends Component{
+    render(){
+
+        return (
+            <div>
+                <h1 id="Header">Carlos Wirawan's Portfolio</h1>
+            </div>
+        );
+    }
+}
+
+export default Portfolio;
