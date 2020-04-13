6578ccc89b83cb3c35b07977cb9c10c34dacb065
cawirawa <57200820+cawirawa@users.noreply.github.com>
2020-04-13 20:38:56 +0700

    Updates

---
+++ precache-manifest.62624c94a1598d4aeb6fa121712917e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c46a08f88aa865e81ade3e72b2a6322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb0503ad2321f8c624e9",
+    "url": "/static/css/main.71af3df6.chunk.css"
+  },
+  {
+    "revision": "bd15567b13b20f95f4cf",
+    "url": "/static/js/2.2e6a2227.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2e6a2227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0503ad2321f8c624e9",
+    "url": "/static/js/main.1976acb2.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "3b5f165b37c858e0111b5ee3a0425f38",
+    "url": "/static/media/carlos.3b5f165b.jpg"
+  },
+  {
+    "revision": "e2028ca9913b479c910597a48cbb143c",
+    "url": "/static/media/space.e2028ca9.jpg"
+  }
+]);
